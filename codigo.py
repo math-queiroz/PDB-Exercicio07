9b5add6787eeb4fe7d0a319dab21f0af37852842
Matheus Queiroz <m-alves7@hotmail.com>
2021-03-23 22:33:31 -0300

    Correção para R2 Ajustado ao invés de R2

--- codigo.py
+++ codigo.py
@@ -17,6 +17,13 @@ dataset = pd.read_csv('winequality-red.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+# Funcão para calcular o R2 Score ajustado
+def r2_score_ajustado(x_test, x_pred):
+    R2 = r2_score(x_test, x_pred)
+    n = len(x)
+    p = len(x[0])
+    return 1-(1-R2)*(n-1)/(n-p-1)
+
 # Reshape
 y = y.reshape(-1, 1)
 
@@ -36,7 +43,7 @@ print(f'Modelo\t\t\t\tR2 Score')
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
 y_pred = regressor.predict(x_test)
-print(f'Regressão Linear:\t\t{r2_score(y_test, y_pred):.5f}')
+print(f'Regressão Linear:\t\t{r2_score_ajustado(y_test, y_pred):.5f}')
 
 # 2.Regressao linear múltipla
 poly = PolynomialFeatures(degree=2)
@@ -44,22 +51,22 @@ x_poly = poly.fit_transform(x_train)
 regressor = LinearRegression()
 regressor.fit(x_poly, y_train)
 y_pred = regressor.predict(poly.transform(x_test))
-print(f'Regressão Linear Múltipla:\t{r2_score(y_test, y_pred):.5f}')
+print(f'Regressão Linear Múltipla:\t{r2_score_ajustado(y_test, y_pred):.5f}')
 
 # 3.Regressao por vetor de suporte
 regressor = SVR(kernel= 'rbf')
 regressor.fit(x_train, y_train.ravel())
 y_pred = regressor.predict(x_test)
-print(f'Regressão Vetor de Suporte:\t{r2_score(y_test, y_pred):.5f}')
+print(f'Regressão Vetor de Suporte:\t{r2_score_ajustado(y_test, y_pred):.5f}')
 
 # 4.Regressao por arvore de decisao
 regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(x_train, y_train)
 y_pred = regressor.predict(x_test)
-print(f'Regressão Arvore de Decisao:\t{r2_score(y_test, y_pred):.5f}')
+print(f'Regressão Arvore de Decisao:\t{r2_score_ajustado(y_test, y_pred):.5f}')
 
 # 5.Regressao por floresta aleatoria
 regressor = RandomForestRegressor(n_estimators=5, random_state=0)
 regressor.fit(x_train, y_train.ravel())
 y_pred = regressor.predict(x_test)
-print(f'Regressão Floresta Aleatoria:\t{r2_score(y_test, y_pred):.5f}')
+print(f'Regressão Floresta Aleatoria:\t{r2_score_ajustado(y_test, y_pred):.5f}')
